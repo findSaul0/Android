554b1622711a1544af71e6d767b713e199ddc98b
findSaul0 <52547719+findSaul0@users.noreply.github.com>
2021-01-24 18:23:43 +0100

    RubricaListView
    aggiornamenti

--- RubricaLisrtView/app/src/main/java/com/example/rubricalisrtview/CustomArrayAdapter.java
+++ RubricaLisrtView/app/src/main/java/com/example/rubricalisrtview/CustomArrayAdapter.java
@@ -1,6 +1,9 @@
 package com.example.rubricalisrtview;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +14,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CustomArrayAdapter extends ArrayAdapter<Contatto> {
@@ -56,7 +60,26 @@ public class CustomArrayAdapter extends ArrayAdapter<Contatto> {
     }
 
     private void removeItem(int p){
-        contattoList.remove(p);
-        notifyDataSetChanged();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Sicuro di eliminare questo contatto?");
+
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                contattoList.remove(p);
+                notifyDataSetChanged();
+            }
+        });
+
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
+
 }
--- RubricaLisrtView/app/src/main/java/com/example/rubricalisrtview/MainActivity.java
+++ RubricaLisrtView/app/src/main/java/com/example/rubricalisrtview/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.rubricalisrtview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -19,14 +20,25 @@ public class MainActivity extends AppCompatActivity {
     List<Contatto> contattoList;
     ListView listView;
 
+    Button button;
     EditText nome;
     EditText cognome;
     EditText numero;
+    String nome_bundle;
+    String cognome_bundle;
+    String numero_bundle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState != null){
+         nome_bundle = savedInstanceState.getString("NOME");
+         cognome_bundle = savedInstanceState.getString("COGNOME");
+         numero_bundle = savedInstanceState.getString("NUMERO");
+        }
+
 
         contattoList = new ArrayList<>();
         contattoList.add(new Contatto("Antonio Fasulo","34393832",R.drawable.ic_launcher_background));
@@ -39,21 +51,26 @@ public class MainActivity extends AppCompatActivity {
 
         listView.setAdapter(adapter);
 
-
-
-    }
-
-    public void InsertNumber(View v){
         nome = findViewById(R.id.tvNome);
         cognome = findViewById(R.id.tvCognome);
         numero = findViewById(R.id.tvTelefono);
+        button = findViewById(R.id.ButtonInsert);
 
-        String nome_edit = nome.getText().toString();
-        String cognome_edit = cognome.getText().toString();
-        String numero_edit = numero.getText().toString();
+        nome.setText(nome_bundle);
+        cognome.setText(cognome_bundle);
+        numero.setText(numero_bundle);
 
-        String completa = nome_edit + " Paol"+ cognome_edit;
+        button.setOnClickListener(event ->{
+            Contatto c = new Contatto(nome.getText().toString()+" "+cognome.getText().toString(),numero.getText().toString(),R.drawable.ic_launcher_background);
+            adapter.add(c);
+        });
+    }
 
-        contattoList.add(new Contatto(completa,numero_edit,R.drawable.ic_launcher_background));
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("NOME",nome.getText().toString());
+        outState.putString("COGNOME",cognome.getText().toString());
+        outState.putString("NUMERO",numero.getText().toString());
     }
 }
