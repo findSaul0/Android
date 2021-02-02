2d4846c38868d1452bd16f098ee5de8e64bc271b
findSaul0 <52547719+findSaul0@users.noreply.github.com>
2021-02-02 15:58:21 +0100

    Autoti&Citazioni

---
+++ FragmentLab1/app/src/main/java/com/example/fragmentlab1/Citazioni.java
@@ -0,0 +1,100 @@
+package com.example.fragmentlab1;
+
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class Citazioni extends Fragment {
+
+    private TextView quoteView = null;
+    private TextView authorView = null;
+    private int current_index = -1;
+    private int quoteArrLen = 0;
+    String[] authors;
+    String[] quotes;
+
+    private void log(String str) {
+        Log.d("MYDEBUG", "QUOTE: " + str);
+    }
+
+    public int getShownIndex() {
+        return current_index;
+    }
+
+    public void showIndex(int newIndex) {
+        if (newIndex < 0 || newIndex >= quoteArrLen)
+            return;
+        current_index = newIndex;
+        authorView.setText(authors[current_index]);
+        quoteView.setText(quotes[current_index]);
+    }
+
+    @Override
+    public void onAttach(Activity a) {
+        super.onAttach(a);
+        log("onAttach");
+        current_index = -1;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        log("onCreateView");
+        return inflater.inflate(R.layout.quote_layout, container, false);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        log("onCreate");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        log("onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        log("onResume");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        log("onDestroy");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        log("onDestroyView");
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        log("onActivityCreated");
+        quoteView = (TextView) getActivity().findViewById(R.id.quoteView);
+        authorView = (TextView) getActivity().findViewById(R.id.authorName);
+        authors = getResources().getStringArray(R.array.Authors);
+        quotes = getResources().getStringArray(R.array.Quotes);
+        quoteArrLen = quotes.length;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        log("onDetach");
+        current_index = -1;
+    }
+}
